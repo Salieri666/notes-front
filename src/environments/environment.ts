1993eadef2d72323fc38c7c7435d8c047dd000ac
Ilya <ilya_789@outlook.com>
2026-02-22 20:10:10 +0400

    Add prod env

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,5 +1,5 @@
 export const environment = {
   production: true,
-  apiUrl: 'http://192.168.0.240:8090/notes-service',
-  keycloakUrl: 'http://192.168.0.240:8082'
+  apiUrl: 'http://5.252.116.172:8090/notes-service',
+  keycloakUrl: 'http://5.252.116.172:8082'
 };
